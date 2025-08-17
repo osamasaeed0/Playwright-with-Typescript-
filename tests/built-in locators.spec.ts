1d83afd1fd12b130a4ccf8849a39d0829e0d6dcc
osamasaeed0 <osamasaeed033640@gmail.com>
2025-08-17 17:41:38 +0500

    Playwright with Typescript

---
+++ tests/built-in locators.spec.ts
@@ -0,0 +1,32 @@
+import{test , expect}  from "@playwright/test"
+import { log } from "console";
+
+test('Examples of locators in Playwright', async ({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/login');
+  // Example of built-in locators
+  // getByRole
+  const LoginId = page.getByRole('button', { name: 'Login' });
+  await expect(LoginId).toBeVisible();
+// getByText
+  const loginText = page.getByText('Login Page');
+  await expect(loginText).toBeVisible();
+
+// getByLabel
+  const usernameLabel = page.getByLabel('Username').fill('tomsmith');
+  const passwordLabel = page.getByLabel('Password').fill('SuperSecretPassword');
+  // Get by CSS selector
+  // by id
+    const username = page.locator('#username');
+    await username.fill('tomsmith');
+    // by attribute
+    const password = page.locator('input[type="password"]');
+    await password.fill('SuperSecretPassword');
+    // by attribute
+    const loginButton = page.locator('button[type="submit"]');
+    await loginButton.click();
+    // Verify successful login
+    const successMessage = page.getByText('You logged into a secure area!');
+    await expect(successMessage).toBeVisible();
+    // Clicking on the logout button
+    // const logoutButton = page.locator("text=Logout").click;
+});    
