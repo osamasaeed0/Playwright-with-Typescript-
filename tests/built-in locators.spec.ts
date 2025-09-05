c6f299c3b2524a0b766657f2d07e629407a177a4
osamasaeed0 <osamasaeed033640@gmail.com>
2025-09-05 15:47:53 +0500

    Add Playwright locator examples

--- tests/built-in locators.spec.ts
+++ tests/built-in locators.spec.ts
@@ -12,21 +12,23 @@ test('Examples of locators in Playwright', async ({ page }) => {
   await expect(loginText).toBeVisible();
 
 // getByLabel
-  const usernameLabel = page.getByLabel('Username').fill('tomsmith');
-  const passwordLabel = page.getByLabel('Password').fill('SuperSecretPassword');
+  // const usernameLabel = page.getByLabel('Username').fill('tomsmith');
+  // const passwordLabel = page.getByLabel('Password').fill('SuperSecretPassword');
   // Get by CSS selector
   // by id
     const username = page.locator('#username');
     await username.fill('tomsmith');
     // by attribute
     const password = page.locator('input[type="password"]');
-    await password.fill('SuperSecretPassword');
+    await password.fill('SuperSecretPassword!');
     // by attribute
-    const loginButton = page.locator('button[type="submit"]');
-    await loginButton.click();
+    await page.locator('button[type="submit"]').click();
+  
+    
     // Verify successful login
     const successMessage = page.getByText('You logged into a secure area!');
     await expect(successMessage).toBeVisible();
     // Clicking on the logout button
-    // const logoutButton = page.locator("text=Logout").click;
+    const logoutButton = page.locator("text=Logout").click; 
+
 });    
